10b99bf28f9b680c16c65cde857d4419a45bb6d6
Emma Hodcroft <emmahodcroft@gmail.com>
2021-02-26 17:41:31 +0100

    add more variants & ORF1a data to shared mutations

--- scripts/mutation_comparison.py
+++ scripts/mutation_comparison.py
@@ -1,5 +1,5 @@
 mutation_comparison = {
-    "20I/501Y.V1": {
+    "20I/ 501Y.V1": {
         "nonsynonymous": [
             "S:H69-",
             "S:V70-",
@@ -10,11 +10,18 @@ mutation_comparison = {
             "S:P681H",
             "S:T716I",
             "S:S982A",
-            "S:D1118H"
+            "S:D1118H",
+
+            "ORF1a:T1001I",
+            "ORF1a:A1708D",
+            "ORF1a:I2230T",
+            "ORF1a:S3675-",
+            "ORF1a:S3676-",
+            "ORF1a:S3677-"
         ]
     },
 
-    "20H/501Y.V2": {
+    "20H/ 501Y.V2": {
         "nonsynonymous": [
             "S:L18F",
             "S:D80A",
@@ -24,11 +31,18 @@ mutation_comparison = {
             "S:E484K",
             "S:N501Y",
             "S:D614G",
-            "S:A701V"
+            "S:A701V",
+
+            "ORF1a:T265I",
+            "ORF1a:K1655N",
+            "ORF1a:K3353R",
+            "ORF1a:S3675-",
+            "ORF1a:S3676-",
+            "ORF1a:S3677-"
         ]
     },
 
-    "20J/501Y.V3": {
+    "20J/ 501Y.V3": {
         "nonsynonymous": [
             "S:L18F",
             "S:T20N",
@@ -40,15 +54,171 @@ mutation_comparison = {
             "S:N501Y",
             "S:D614G",
             "S:H655Y",
-            "S:T1027I"
+            "S:T1027I",
+
+            "ORF1a:S1188L",
+            "ORF1a:K1795Q",
+            "ORF1a:S3675-",
+            "ORF1a:S3676-",
+            "ORF1a:S3677-"
         ]
     },
 
-    "20B/S.484K": {
+#    "20B/S.484K": {    #temporarily removed as gets too wide on screen!
+#        "nonsynonymous": [
+#            "S:E484K",
+#            "S:D614G",
+#            "S:V1176F"
+#        ]
+#    },
+
+    "B.1.427/ B.1.429": {
+        "nonsynonymous": [
+            "S:S13I",
+            "S:W152C",
+            "S:L452R",
+            "S:D614G",
+
+            "ORF1a:T265I",
+            "ORF1a:S3158T"
+        ]
+    },
+
+    "B.1.526 (NY1)": {
+        "nonsynonymous": [
+            "S:L5F",
+            "S:T95I",
+            "S:D253G",
+            "S:E484K",
+            "S:D614G",
+            "S:A701V",
+
+            "ORF1a:T265I",
+            "ORF1a:L3201P",
+            "ORF1a:S3675-",
+            "ORF1a:S3676-",
+            "ORF1a:S3677-"
+        ]
+    },
+
+    "B.1.526 (NY2)": {
+        "nonsynonymous": [
+            "S:L5F",
+            "S:T95I",
+            "S:D253G",
+            "S:S477N",
+            "S:D614G",
+            "S:A701V",
+
+            "ORF1a:T265I",
+            "ORF1a:L3201P",
+            "ORF1a:P3504S",
+            "ORF1a:S3675-",
+            "ORF1a:S3676-",
+            "ORF1a:S3677-"
+        ]
+    },
+
+    "B.1.1.316": {
+        "nonsynonymous": [
+            "S:W152L",
+            "S:E484K",
+            "S:D614G",
+            "S:G769V"
+        ]
+    },
+
+    "B.1.1.207": {
         "nonsynonymous": [
             "S:E484K",
             "S:D614G",
-            "S:V1176F"
+            "S:P681H",
+
+            "ORF1a:S428G",
+            "ORF1a:A3571V"
+        ]
+    },
+
+    "B.1.1.318": {
+        "nonsynonymous": [
+            "S:T95I",
+            "S:Y144-",
+            "S:E484K",
+            "S:D614G",
+            "S:P681H",
+            "S:D796H",
+
+            "ORF1a:E1196V",
+            "ORF1a:K2511N",
+            "ORF1a:T2936I",
+            "ORF1a:A3209V",
+            "ORF1a:T3284I",
+            "ORF1a:S3675-",
+            "ORF1a:S3676-",
+            "ORF1a:S3677-"
+        ]
+    },
+
+    "B.1.525": {
+        "nonsynonymous": [
+            "S:Q52R",
+            "S:A67V",
+            "S:H69-",
+            "S:V70-",
+            "S:Y144-",
+            "S:E484K",
+            "S:D614G",
+            "S:Q677H",
+            "S:F888L",
+
+            "ORF1a:T2007I",
+            "ORF1a:S3675-",
+            "ORF1a:S3676-",
+            "ORF1a:S3677-"
+        ]
+    },
+
+    "19B/ S:501Y": {
+        "nonsynonymous": [
+            "S:H69-",
+            "S:V70-",
+            "S:N501T",
+            "S:H655Y",
+
+            "ORF1a:V86F",
+            "ORF1a:A3529T"
+        ]
+    },
+
+    "19B/ S:501T": {
+        "nonsynonymous": [
+            "S:L18F",
+            "S:L452R",
+            "S:N501Y",
+            "S:A653V",
+            "S:H655Y",
+            "S:Q677H",
+            "S:D796Y",
+            "S:G1219V",
+
+            "ORF1a:P286L",
+            "ORF1a:D2980G",
+            "ORF1a:N3651S"
+        ]
+    },
+
+    "19B/ S:484K": {
+        "nonsynonymous": [
+            "S:R102I",
+            "S:F157L",
+            "S:E484K",
+            "S:Q613H",
+            "S:P681R",
+
+            "ORF1a:L1559F",
+            "ORF1a:M3655I",
+            "ORF1a:L3667F",
+            "ORF1a:M3752I"
         ]
     }
 
